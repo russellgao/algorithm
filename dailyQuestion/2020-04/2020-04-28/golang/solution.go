007da865284048acae75769fbb7255e56e0b758f
gaoweizong <gaoweizong@hd123.com>
2020-04-28 21:37:58 +0800

    init

---
+++ dailyQuestion/2020-04/2020-04-28/golang/solution.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := []int{4, 5, 6, 7, 0, 1, 2}
+	b := 0
+	c := search(a, b)
+	fmt.Println(c)
+}
+
+func search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := (left + right) >> 1
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[0] <= nums[mid] {
+			if nums[0] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target > nums[mid] && target <= nums[len(nums)-1] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
