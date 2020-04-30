2416dc2b6fa1a46f6fe7f984929e386c25315d5f
gaoweizong <gaoweizong@hd123.com>
2020-04-30 21:52:37 +0800

    链表反转

---
+++ dailyQuestion/2020-04/04-30/python/solution.py
@@ -0,0 +1,70 @@
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# =============================================
+# 方法一
+# 递归
+#
+# =============================================
+
+def reverseBetween1(head, m, n) :
+    """
+    :type head: ListNode
+    :type m: int
+    :type n: int
+    :rtype: ListNode
+    """
+
+
+# =============================================
+# 方法二
+# 迭代
+#
+# =============================================
+
+def reverseBetween2(head, m, n):
+    """
+    :type head: ListNode
+    :type m: int
+    :type n: int
+    :rtype: ListNode
+    """
+    if not head :
+        return None
+    pre, current = None, head
+    while m > 1 :
+        pre = current
+        current = current.next
+        n,m = n - 1, m -1
+    con, tail = pre,current
+    while n :
+        tmp = current.next
+        current.next = pre
+        pre = current
+        current = tmp
+        n -= 1
+    if con:
+        con.next = pre
+    else :
+        head = pre
+    tail.next = current
+    return head
+
+# 时间复杂度: O(N) 考虑包含 NN 个结点的链表。对每个节点最多会处理
+# （第 nn 个结点之后的结点不处理）
+# 空间复杂度: O(1)。我们仅仅在原有链表的基础上调整了一些指针，只使用了 O(1)O(1) 的额外存储空间来获得结果。
+
+
+if __name__ == '__main__':
+    node = ListNode(1)
+    node.next = ListNode(2)
+    node.next.next = ListNode(3)
+    node.next.next.next = ListNode(4)
+    node.next.next.next.next = ListNode(5)
+    result = reverseBetween2(node, 2,4)
+    print()
