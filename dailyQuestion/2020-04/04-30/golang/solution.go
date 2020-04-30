27424651637dd095e3d24565c30f67f05393e43e
gaoweizong <gaoweizong@hd123.com>
2020-04-30 22:07:30 +0800

    链表反转

---
+++ dailyQuestion/2020-04/04-30/golang/solution.go
@@ -0,0 +1,36 @@
+
+Definition for singly-linked list.
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+// 方法二 
+// 迭代
+func reverseBetween(head *ListNode, m int, n int) *ListNode {
+    if head == nil {
+        return nil
+    }
+    pre := &ListNode{}
+    current := head
+    for m > 1 {
+        pre = current
+        current = current.Next
+        m , n = m - 1, n - 1
+    }
+    con, tail := pre, current
+    for n > 0 {
+        tmp := current.Next
+        current.Next = pre
+        pre = current
+        current = tmp 
+        n -= 1
+    }
+    if con != nil && con.Next != nil {
+        con.Next = pre
+    } else {
+        head = pre
+    }
+    tail.Next = current
+    return head
+}
