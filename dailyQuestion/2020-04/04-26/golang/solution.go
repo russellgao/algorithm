408c5a532aaa37516b3e754f91021479ed25c5cd
gaoweizong <gaoweizong@hd123.com>
2020-05-04 22:01:13 +0800

    最大子序列问题

--- dailyQuestion/2020-04/04-26/golang/solution.go
+++ dailyQuestion/2020-04/04-26/golang/solution.go
@@ -9,7 +9,7 @@ func main() {
 }
 
 // 方法一
-// 分治法
+// 动态规划
 func maxSubArray(nums []int) int {
 	max := nums[0]
 	for i := 1; i < len(nums); i++ {
@@ -22,3 +22,42 @@ func maxSubArray(nums []int) int {
 	}
 	return max
 }
+
+// 方法二
+// 分治法
+
+
+func maxSubArray_2(nums []int) int {
+	return get(nums, 0, len(nums)-1).mSum
+}
+
+func pushUp(left, right Status) Status {
+	iSum := left.iSum + right.iSum
+	lSum := max(left.lSum, left.iSum+right.lSum)
+	rSum := max(right.rSum, right.iSum+left.rSum)
+	mSum := max(max(left.mSum, right.mSum), left.rSum+right.lSum)
+	return Status{iSum: iSum, lSum: lSum, rSum: rSum, mSum: mSum}
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func get(nums []int, left, right int) Status {
+	if left == right {
+		return Status{lSum: nums[left], rSum: nums[left], mSum: nums[left], iSum: nums[left]}
+	}
+	m := (left + right) >> 1
+	lSub := get(nums, left, m)
+	rSub := get(nums, m+1, right)
+	return pushUp(lSub, rSub)
+}
+
+type Status struct {
+	lSum, rSum, mSum, iSum int
+}
+
+
