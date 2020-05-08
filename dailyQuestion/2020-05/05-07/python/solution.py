8ea7383f7ec42ad1f56189182ccd89da232f0f0a
gaoweizong <gaoweizong@hd123.com>
2020-05-08 08:35:43 +0800

    5.7 二叉搜索子树最大键值和

--- dailyQuestion/2020-05/05-07/python/solution.py
+++ dailyQuestion/2020-05/05-07/python/solution.py
@@ -9,10 +9,20 @@ def fastest_path(nums) :
         for j in range(len_i):
             if j == 0 :
                 nums[i][j] = nums[i - 1][j] + nums[i][j]
-            elif j == len_i:
+            elif j == len_i-1:
                 nums[i][j] = nums[i - 1][j - 1] + nums[i][j]
             else :
                 tmp = max(nums[i][j] + nums[i - 1][j - 1], nums[i][j] + nums[i - 1][j])
                 nums[i][j] = tmp
     result = max(nums[len(nums) - 1])
     return result
+
+if __name__ == "__main__" :
+    nums = [
+[7],
+[3, 8 ] ,
+[8, 1, 0 ],
+[2, 7, 4, 4 ],
+[4 ,5 ,2, 6, 5]]
+    result = fastest_path(nums)
+    print(result)
