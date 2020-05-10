529316b452bebbf1468c06bd94cbca5356dfd37e
gaoweizong <gaoweizong@hd123.com>
2020-05-10 20:51:16 +0800

    装饰器

---
+++ ProgrammingLanuage/python/Decorator/example_timet.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import time
+from datetime import datetime
+
+
+def timet(*args, **kwargs) :
+    print("装饰器参数 =====")
+    for item_a in args :
+        print(item_a)
+    for item_k ,item_v in kwargs.iteritems() :
+        print(item_k,item_v)
+    print("装饰器参数------")
+
+    def outter(func) :
+        print("otter=====")
+        def inner(*args, **kwargs) :
+            start_time = datetime.now()
+            print("inner====")
+            func(*args, **kwargs)
+            end_time = datetime.now()
+            print("total cost {}".format(end_time - start_time))
+        return inner
+    return outter
+
+@timet("p1","p2",p3="oirei",p4="uryhd")
+def p_p1(a1,a2,a3 = "pkfjf") :
+    print("=====")
+    print(a1)
+    print(a2)
+    print(a3)
+    print("======")
+    time.sleep(4)
+
+p_p1("a1111111","a22222")
+
+print()
