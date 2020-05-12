7285785f6596e0659a36a7a830bedc3866316688
gaoweizong <gaoweizong@hd123.com>
2020-05-12 21:36:08 +0800

    5.12 最小栈

---
+++ dailyQuestion/2020-05/05-12/python/solution.py
@@ -0,0 +1,27 @@
+import math
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = [math.inf]
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.min_stack.append(min(x,self.min_stack[-1]))
+
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
