ca899bd58482f4dcab00b674d75765e1acf8f005
gaoweizong <gaoweizong@hd123.com>
2020-05-17 11:54:52 +0800

    有符号数和无符号数举例

--- data_structure/bit/python/signed_example.py
+++ data_structure/bit/python/signed_example.py
@@ -1,6 +1,14 @@
-
 # 有符号数和无符号数举例
-num1 = int("11",2)
-num2 = ~4
+
+import ctypes
+
+num = 17039472050328044269
+# num = 90
+
+# 转换成有符号数
+a = ctypes.c_int64(num).value
+
+# 转换成无符号数
+b = ctypes.c_uint64(a).value
 
 print()
