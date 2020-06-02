aef7e69736d119766d5ba67269ac45d998f97e02
gaoweizong <gaoweizong@hd123.com>
2020-06-02 09:32:18 +0800

    6.1 排序链表

--- dailyQuestion/2020-06/06-01/python/solution_items.py
+++ dailyQuestion/2020-06/06-01/python/solution_items.py
@@ -8,42 +8,47 @@ class ListNode:
 
 # 迭代
 def sortList(head: ListNode) -> ListNode:
-    h, length, intv = head, 0, 1
-    while h:
-        h, length = h.next, length + 1
-    res = ListNode(0)
-    res.next = head
-    # merge the list in different intv.
-    while intv < length:
-        pre, h = res, res.next
-        while h:
-            # get the two merge head `h1`, `h2`
-            h1, i = h, intv
-            while i and h:
-                h, i = h.next, i - 1
-            if i:
-                break  # no need to merge because the `h2` is None.
-            h2, i = h, intv
-            while i and h:
-                h, i = h.next, i - 1
-            c1, c2 = intv, intv - i  # the `c2`: length of `h2` can be small than the `intv`.
-            # merge the `h1` and `h2`.
-            while c1 and c2:
-                if h1.val < h2.val:
-                    pre.next = h1
-                    h1 = h1.next
-                    c1 = c1 - 1
-                else:
+    head_len = 0
+    invc = 1
+    h = head
+    while h :
+        head_len += 1
+        h = h.next
+    result = ListNode(0)
+    result.next = head
+    while invc <= head_len :
+        pre = result
+        h = result.next
+        while h :
+            h1 ,i = h , invc
+            while i and h :
+                i -= 1
+                h = h.next
+            if i :
+                break
+            h2, i = h, invc
+            while i and h :
+                i -= 1
+                h = h.next
+            c1, c2 = invc, invc-i
+            while c1 and c2 :
+                if h1.val > h2.val :
                     pre.next = h2
                     h2 = h2.next
-                    c2 = c2 -1
+                    c2 -= 1
+                else :
+                    pre.next = h1
+                    h1 = h1.next
+                    c1 -= 1
                 pre = pre.next
             pre.next = h1 if c1 else h2
-            while c1 > 0 or c2 > 0:
-                pre, c1, c2 = pre.next, c1 - 1, c2 - 1
+            while c1 > 0 or c2 > 0 :
+                pre = pre.next
+                c1 -= 1
+                c2 -= 1
             pre.next = h
-        intv = intv << 1
-    return res.next
+        invc <<= 1
+    return result.next
 
 
 if __name__ == "__main__" :
