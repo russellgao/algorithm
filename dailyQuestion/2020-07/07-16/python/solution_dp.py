81cc8d221671a71fa1b7f25d264ee1983520565f
gaoweizong <gaoweizong@hd123.com>
2020-07-16 21:35:36 +0800

    7.16 不同的二叉搜索树

---
+++ dailyQuestion/2020-07/07-16/python/solution_dp.py
@@ -0,0 +1,13 @@
+
+def numTrees(n: int) -> int:
+    G = [0] * (n+1)
+    G[0] = G[1] = 1
+    for i in range(2,n+1) :
+        for j in range(1,i+1) :
+            G[i] += G[j-1] * G[i-j]
+    return G[n]
+
+if __name__ == "__main__" :
+    n = 3
+    result = numTrees(n)
+    print(result)
