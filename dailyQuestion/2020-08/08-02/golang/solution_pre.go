e5b33a9adaa1c0fca4e2b88a5385beb29bbc185b
gaoweizong <gaoweizong@hd123.com>
2020-08-02 12:04:33 +0800

    8.2 二叉树展开为链表

---
+++ dailyQuestion/2020-08/08-02/golang/solution_pre.go
@@ -0,0 +1,38 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func flatten(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	var pre *TreeNode
+	stack := []*TreeNode{root}
+	for len(stack) != 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if pre != nil {
+			pre.Left = nil
+			pre.Right = node
+		}
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		}
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+		}
+		pre = node
+	}
+}
+
+func main() {
+	root := &TreeNode{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 5}}
+	root.Left.Left = &TreeNode{Val: 3}
+	root.Left.Right = &TreeNode{Val: 4}
+	root.Right.Right = &TreeNode{Val: 6}
+	flatten(root)
+}
