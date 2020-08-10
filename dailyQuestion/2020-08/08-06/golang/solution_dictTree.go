6c5ba80245eb7ea287b61c934bc96e710ab1389d
gaoweizong <gaoweizong@hd123.com>
2020-08-10 19:57:12 +0800

    8.6 回文对

--- dailyQuestion/2020-08/08-06/golang/solution_dictTree.go
+++ dailyQuestion/2020-08/08-06/golang/solution_dictTree.go
@@ -1,35 +1,6 @@
-= 回文对
-:toc:
-:toc-title:
-:toclevels:
-:sectnums:
+package main
 
-== 说明
-给定一组 互不相同 的单词， 找出所有不同 的索引对(i, j)，使得列表中的两个单词， words[i] + words[j] ，可拼接成回文串。
-
-示例 1：
-```
-输入：["abcd","dcba","lls","s","sssll"]
-输出：[[0,1],[1,0],[3,2],[2,4]]
-解释：可拼接成的回文串为 ["dcbaabcd","abcddcba","slls","llssssll"]
-```
-示例 2：
-```
-输入：["bat","tab","cat"]
-输出：[[0,1],[1,0]]
-解释：可拼接成的回文串为 ["battab","tabbat"]
-
-```
-
-== 参考
-- https://leetcode-cn.com/problems/palindrome-pairs/
-
-== 题解
-
-image:images/1.jpg[]
-
-- 字典树
-```go
+import "fmt"
 
 type Node struct {
 	ch   [26]int
@@ -99,7 +70,9 @@ func isPalindrome(s string, left, right int) bool {
 	}
 	return true
 }
-```
 
-image:images/2.jpg[]
-
+func main() {
+	nums := []string{"abcd", "dcba", "lls", "s", "sssll"}
+	result := palindromePairs(nums)
+	fmt.Println(result)
+}
