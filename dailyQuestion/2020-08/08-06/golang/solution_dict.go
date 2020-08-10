bbc9dc19f3714f13256fe06d8c92f15fcc1bf61f
gaoweizong <gaoweizong@hd123.com>
2020-08-10 19:58:00 +0800

    8.6 回文对

--- dailyQuestion/2020-08/08-06/golang/solution_dict.go
+++ dailyQuestion/2020-08/08-06/golang/solution_dict.go
@@ -2,32 +2,40 @@ package main
 
 import "fmt"
 
-type Node struct {
-	ch   [26]int
-	flag int
+func main() {
+	nums := []string{"abcd", "dcba", "lls", "s", "sssll"}
+	result := palindromePairs(nums)
+	fmt.Println(result)
+
 }
-
-var tree []Node
-
 func palindromePairs(words []string) [][]int {
-	tree = []Node{Node{[26]int{}, -1}}
+	wordsRev := []string{}
+	indices := map[string]int{}
+
 	n := len(words)
-	for i := 0; i < n; i++ {
-		insert(words[i], i)
+	for _, word := range words {
+		wordsRev = append(wordsRev, reverse(word))
 	}
+	for i := 0; i < n; i++ {
+		indices[wordsRev[i]] = i
+	}
+
 	ret := [][]int{}
 	for i := 0; i < n; i++ {
 		word := words[i]
 		m := len(word)
+		if m == 0 {
+			continue
+		}
 		for j := 0; j <= m; j++ {
 			if isPalindrome(word, j, m-1) {
-				leftId := findWord(word, 0, j-1)
+				leftId := findWord(word, 0, j-1, indices)
 				if leftId != -1 && leftId != i {
 					ret = append(ret, []int{i, leftId})
 				}
 			}
 			if j != 0 && isPalindrome(word, 0, j-1) {
-				rightId := findWord(word, j, m-1)
+				rightId := findWord(word, j, m-1, indices)
 				if rightId != -1 && rightId != i {
 					ret = append(ret, []int{rightId, i})
 				}
@@ -37,29 +45,11 @@ func palindromePairs(words []string) [][]int {
 	return ret
 }
 
-func insert(s string, id int) {
-	add := 0
-	for i := 0; i < len(s); i++ {
-		x := int(s[i] - 'a')
-		if tree[add].ch[x] == 0 {
-			tree = append(tree, Node{[26]int{}, -1})
-			tree[add].ch[x] = len(tree) - 1
-		}
-		add = tree[add].ch[x]
+func findWord(s string, left, right int, indices map[string]int) int {
+	if v, ok := indices[s[left:right+1]]; ok {
+		return v
 	}
-	tree[add].flag = id
-}
-
-func findWord(s string, left, right int) int {
-	add := 0
-	for i := right; i >= left; i-- {
-		x := int(s[i] - 'a')
-		if tree[add].ch[x] == 0 {
-			return -1
-		}
-		add = tree[add].ch[x]
-	}
-	return tree[add].flag
+	return -1
 }
 
 func isPalindrome(s string, left, right int) bool {
@@ -71,8 +61,11 @@ func isPalindrome(s string, left, right int) bool {
 	return true
 }
 
-func main() {
-	nums := []string{"abcd", "dcba", "lls", "s", "sssll"}
-	result := palindromePairs(nums)
-	fmt.Println(result)
+func reverse(s string) string {
+	n := len(s)
+	b := []byte(s)
+	for i := 0; i < n/2; i++ {
+		b[i], b[n-i-1] = b[n-i-1], b[i]
+	}
+	return string(b)
 }
