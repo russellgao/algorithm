a6670e9e2215e57b7920f9850cf57150659f45f2
gaoweizong <gaoweizong@hd123.com>
2020-08-31 09:07:14 +0800

    8.29 电话号码的字母组合

--- dailyQuestion/2020-08/08-29/golang/solution.go
+++ dailyQuestion/2020-08/08-29/golang/solution.go
@@ -1,35 +1,13 @@
-= 电话号码的字母组合
-:toc:
-:toc-title:
-:toclevels:
-:sectnums:
+package main
 
-== 说明
-给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+import "fmt"
 
-给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+func main() {
+	digits := "234"
+	result := letterCombinations(digits)
+	fmt.Println(result)
+}
 
-image:images/1.png[]
-
-
-示例:
-```
-输入："23"
-输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
-```
-说明:
-
-- 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
-
-
-== 参考
-- https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
-
-== 题解
-=== 回溯
-image:images/2.png[]
-
-```go
 var phoneMappings map[byte]string = map[byte]string{
 	'2': "abc",
 	'3': "def",
@@ -64,8 +42,3 @@ func backtrace(digits string, index int, combination string) {
 		}
 	}
 }
-```
-
-image:images/3.png[]
-
-
