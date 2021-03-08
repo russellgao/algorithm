5431ae00431788de43b6eb06bd6f56b076a8a9b0
gaoweizong <gaoweizong@hd123.com>
2021-03-08 23:02:57 +0800

    分割回文串 II

---
+++ dailyQuestion/2021/2021-03/03-08/golang/solution.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	res := minCut("ab")
+	fmt.Println(res)
+}
+
+func minCut(s string) int {
+	n := len(s)
+	if n == 0 {
+		return 0
+	}
+	g := make([][]bool, n)
+	for i := 0; i < n; i++ {
+		g[i] = make([]bool, n)
+		for j := 0; j < n; j++ {
+			g[i][j] = true
+		}
+	}
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			g[i][j] = s[i] == s[j] && g[i+1][j-1]
+		}
+	}
+	f := make([]int, n)
+	for i := 0; i < n; i++ {
+		if g[0][i] {
+			continue
+		}
+		f[i] = math.MaxInt64
+		for j := 0; j < i; j++ {
+			if g[j+1][i] && f[j]+1 < f[i] {
+				f[i] = f[j] + 1
+			}
+		}
+	}
+	return f[n-1]
+}
